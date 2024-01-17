70cba6b74460f0633820660569be6d1d4598a855
Anucha.tk <anuchataowkaen@gmail.com>
2024-01-18 00:10:00 +0700

    update init-mongo.js

---
+++ README.md
@@ -0,0 +1,25 @@
+# EXPRESS-REACT-GRAPHQL
+
+## Tech-stacks
+
+- express
+- react(vite)
+- graphql(back-end)
+- apollo(front-end)
+- mongoose(database)
+- docker
+- basic CRUD
+
+## Start
+
+### run docker
+
+```bash
+make start-mongo
+```
+
+### run back-end and front-end
+
+```bash
+yarn dev
+```
--- server/commons/database/db.ts
+++ server/commons/database/db.ts
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 import { logger } from "../logger/logger";
 
 const connectDB = async () => {
-  const uri = `mongodb://${process.env.DATABASE_USER}:${process.env.DATABASE_USER_PWD}@127.0.0.1:27017/${process.env.DATABASE_NAME}`;
+  const uri = `mongodb://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@127.0.0.1:27017/${process.env.MONGO_DB}`;
   const conn = await mongoose.connect(uri);
 
   logger.info(`MongoDB Connected: ${conn.connection.host}`);
